8957110e1a32d7770eff1313b825b8d661bf4495
Talha1818 <muhammadtalha171717@gmail.com>
2025-07-07 17:25:14 +0500

    cicd implementation

--- test_app.py
+++ test_app.py
@@ -4,4 +4,4 @@ def test_home():
     client = app.test_client()
     response = client.get('/')
     assert response.status_code == 200
-    assert b"Hello World from Muhammad" in response.data
+    assert b"Hello World from Muhammad Talha" in response.data
