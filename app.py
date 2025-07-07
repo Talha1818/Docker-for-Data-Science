8d88c0dba32d4c0a9a86cacc8a4d7fd76cb7155c
Talha1818 <muhammadtalha171717@gmail.com>
2025-07-07 17:18:31 +0500

    cicd implementation

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
 ## flask app for hello world
 
 from flask import Flask
-import numpy as np
-import pandas as pd
 
 app=Flask(__name__)
 
 @app.route('/',methods=['GET'])
 def home():
-    return "Hello World"
+    return "Hello World from Muhammad"
 
 
 
